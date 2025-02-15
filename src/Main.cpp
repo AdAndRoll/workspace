672acd5ff0831b2ec763fec8307991a01ff6002b
AdAndRoll <vanovano03@mail.ru>
2025-02-15 23:18:23 +0000

    Сохраняю иfor branch

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "echo_server.h"
+#include "Calculator.h"
 int main() {
     std::cout << "Hello, World!" << std::endl;
-
+    run_calculator();
     start_http_server();
 
     return 0;
